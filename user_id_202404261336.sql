74093147d384db71596b8bca9744ec34f9951b28
Nikenrhyngty <161337491+Nikenrhyngty@users.noreply.github.com>
2024-04-26 13:37:13 +0700

    Database Users

---
+++ user_id_202404261336.sql
@@ -0,0 +1,35 @@
+INSERT INTO users.user_id (user_id,name,email,phone,gender,address) VALUES
+	 (1,'Stephany Rin','step_rin@gmail.com','082347578','Female','London'),
+	 (2,'William Louis','will_lou@gmail.com','023575578','Male','Los Angeles, CA'),
+	 (3,'Harry Styles','harrystyles@gmail.com','034567786','Male','London'),
+	 (4,'Robert Timothi','timothis@gmail.com','036882974','Male','Canada'),
+	 (5,'Jeanna Firna','jean@gmail.com','023454365','Female','Iceland'),
+	 (6,'Sammy Kim','samsammyy@gmail.com','024723659','Male','Los Angeles, CA'),
+	 (7,'Leo Chang','lechang@gmail.com','032231115','Male','New York'),
+	 (8,'Emily Christian','emily@gmail.com','012414446','Female','Paris'),
+	 (9,'Big Matthew','bigmatt@gmail.com','021145588','Male','Los Angeles, CA'),
+	 (10,'Billie Austin','billyreal@gmail.com','021158990','Female','Las Vegas');
+INSERT INTO users.user_id (user_id,name,email,phone,gender,address) VALUES
+	 (11,'Dakota Ace','realace@gmail.com','012324789','Male','Copenhagen'),
+	 (12,'Yian Im','yian@gmail.com','013598323','Female','Hongkong'),
+	 (13,'Samantha Adriana','samantha@gmail.com','043567899','Female','Sienna'),
+	 (14,'Davide Morr','davidemorr@gmail.com','045267888','Male','Firenze'),
+	 (15,'Ruby Jane','rubyjane@gmail.com','031245986','Female','Firenze'),
+	 (16,'Britt Florence','britt@gmail.com','012455499','Female','Rome'),
+	 (17,'Christian Yofis','yofis@gmail.com','025897252','Male','Prague'),
+	 (18,'Salma Ann','salmaann@gmail.com','012040128','Female','Vienna'),
+	 (19,'Moris Stephan','morstep@gmail.com','021948918','Male','Madrid'),
+	 (20,'Adele','adele@gmail.com','021408411','Female','Budapest');
+INSERT INTO users.user_id (user_id,name,email,phone,gender,address) VALUES
+	 (21,'Giorgino Paulus','giorgino@gmail.com','012015236','Male','Barcelona'),
+	 (22,'Hardin Kith','hardin@gmail.com','014164599','Male','Amsterdam'),
+	 (23,'Miela Mar','miela@gmail.com','012234378','Female','Stockholm'),
+	 (24,'Ruby Jane','jane@gmail.com','018943998','Female','Athens'),
+	 (25,'Victor ','victor@gmail.com','028419742','Male','Kalambaka'),
+	 (26,'Taylor Swift','taylor@gmail.com','025989678','Female','Moscow'),
+	 (27,'Jeremy Zucker','jeremy@gmail.com','02943856','Male','Warsaw'),
+	 (28,'Zidane ','zidane@gmail.com','0218410892','Male','Brussel'),
+	 (29,'Normani','normani@gmail.com','03295822','Female','Barcelona'),
+	 (30,'Zendaya','zendaya@gmail.com','021124856','Female','Stockholm');
+INSERT INTO users.user_id (user_id,name,email,phone,gender,address) VALUES
+	 (31,'Cardiana Safitri','cardib@gmail.com','23985105','Female','Mexico');
